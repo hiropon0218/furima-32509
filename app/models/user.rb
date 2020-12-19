d07c9d7c1cbb28db41a6761c8ba0edb065ed4351
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-19 15:49:41 +0900

    本名入力のバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
 
   validates :nickname,     presence: true
   validates :birthday,     presence: true
   
  
   with_options presence: true do        
-    validates :last_name_kanji,        format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}     
-    validates :first_name_kanji,       format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}    
+    validates :last_name_kanji,        format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}     
+    validates :first_name_kanji,       format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}    
     validates :last_name_katakana,     format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}  
     validates :first_name_katakana,    format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
     validates :password,               format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width alphanumeric characters."}
