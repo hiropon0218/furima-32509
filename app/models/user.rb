5b9116b4a60121f38bcd59dc90c5d67cb5f9c9a0
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-19 16:17:42 +0900

    重複しているバリデーションをwith_optinsでまとめる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-
-  validates :nickname,     presence: true
-  validates :birthday,     presence: true
+ with_options presence: true do
+  validates :nickname
+  validates :birthday
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width alphanumeric characters."}
+ end
   
- 
-  with_options presence: true do        
-    validates :last_name_kanji,        format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}     
-    validates :first_name_kanji,       format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}    
-    validates :last_name_katakana,     format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}  
-    validates :first_name_katakana,    format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."}
-    validates :password,               format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width alphanumeric characters."}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."} do      
+    validates :last_name_kanji 
+    validates :first_name_kanji
   end
 
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width characters."} do
+    validates :last_name_katakana  
+    validates :first_name_katakana
+  end
 
 end
