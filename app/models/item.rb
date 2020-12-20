0e763e91be75ff7c9c626cf85470bfb8ad85421d
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-20 15:11:16 +0900

    itemモデルにおけるバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,14 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :imege
+
+  validetes :item_name,         presence: true, length: { maximum: 40 }
+  validates :explanation,       presence: true, length: { maximum: 1000 }
+  validates :category_id,       presence: true
+  validates :quality_id,        presence: true
+  validates :shipping_cost_id,  presence: true
+  validates :prefecture_id,     presence: true
+  validatas :ship_date_id,      presence: true
+  validates :price,             presence: true
+  validates :imege,             presence: true
 end
