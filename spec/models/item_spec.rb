1bffdccd8d1f336746ed1d288867b7df9aadc9a0
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-22 15:24:45 +0900

    不足していたテストの追加
    ActiveHashを仕様したカラムのidが1の場合。
    販売価格に関して3種類。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -36,17 +36,35 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Category is not a number')
       end
+       
+      it 'カテゴリーの選択idが1(---)の時' do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
 
       it '商品の状態が選択されていない時' do
         @item.quality_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Quality is not a number')
+        expect(@item.errors.full_messages).to include("Quality is not a number")
+      end
+
+      it '商品の状態の選択idが1(---)の時' do
+        @item.quality_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Quality must be other than 1")
       end
 
       it '配送料の負担が選択されていない時' do
         @item.shipping_cost_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Shipping cost is not a number')
+        expect(@item.errors.full_messages).to include("Shipping cost is not a number")
+      end
+
+      it '配送料の負担の選択idが1(---)の時' do
+        @item.shipping_cost_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost must be other than 1")
       end
 
       it '発送元の地域が選択されていない時' do
@@ -55,10 +73,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Prefecture is not a number')
       end
 
+      it '発送元の地域の選択idが1(---)の時' do
+        @item.prefecture_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+
       it '発送までの日数が選択されていない時' do
         @item.ship_date_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('Ship date is not a number')
+      end
+
+      it '発送までの日数の選択idが1(---)の時' do
+        @item.ship_date_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship date must be other than 1")
       end
 
       it '販売価格が存在しない時' do
@@ -70,13 +100,31 @@ RSpec.describe Item, type: :model do
       it '販売価格が299円以下の時' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it '販売価格が10,000,000円以上の時' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it '販売価格が全角文字の時' do
+        @item.price = 'イチに三'
+        @item.valid? 
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '販売価格が半角英数字混合の時' do
+        @item.price = '1a2b3c'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '販売価格が半角英語だけの時' do
+        @item.price = 'OneTwoThree'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
       end
     end
   end
