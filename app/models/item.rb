e310922a468294914f5d3ded0233bac3be725f40
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-22 15:59:32 +0900

    バリデーションをwith_optionsでまとめました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,13 +8,19 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :item_name,         presence: true
-  validates :explanation,       presence: true
-  validates :category_id,       numericality: { other_than: 1 }
-  validates :quality_id,        numericality: { other_than: 1 }
-  validates :shipping_cost_id,  numericality: { other_than: 1 }
-  validates :prefecture_id,     numericality: { other_than: 1 }
-  validates :ship_date_id,      numericality: { other_than: 1 }
-  validates :price,             presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :image,             presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :quality_id        
+    validates :shipping_cost_id  
+    validates :prefecture_id    
+    validates :ship_date_id
+  end      
+  
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :explanation
+    validates :price,         numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+  
 end
