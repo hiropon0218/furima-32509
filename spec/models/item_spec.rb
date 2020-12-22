81255b974ad243b81c04961948bf88e6952763ce
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-22 16:22:23 +0900

    不要なクオーテーションの削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Item, type: :model do
       end
        
       it 'カテゴリーの選択idが1(---)の時' do
-        @item.category_id = "1"
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category must be other than 1")
       end
@@ -50,7 +50,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '商品の状態の選択idが1(---)の時' do
-        @item.quality_id = "1"
+        @item.quality_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Quality must be other than 1")
       end
@@ -62,7 +62,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '配送料の負担の選択idが1(---)の時' do
-        @item.shipping_cost_id = "1"
+        @item.shipping_cost_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping cost must be other than 1")
       end
@@ -74,7 +74,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '発送元の地域の選択idが1(---)の時' do
-        @item.prefecture_id = "1"
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
       end
@@ -86,7 +86,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '発送までの日数の選択idが1(---)の時' do
-        @item.ship_date_id = "1"
+        @item.ship_date_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship date must be other than 1")
       end
@@ -98,13 +98,13 @@ RSpec.describe Item, type: :model do
       end
 
       it '販売価格が299円以下の時' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
 
       it '販売価格が10,000,000円以上の時' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
