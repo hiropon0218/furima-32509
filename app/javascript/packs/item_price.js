72f7d4d0ee29c97b9e40422700b2aa95604e3933
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-22 15:39:20 +0900

    デバック時に仕様した記述の削除

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -2,14 +2,11 @@ window.addEventListener('load',() => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input",()=> {
     const inputValue = priceInput.value;
-    console.log(inputValue);
     
     const addTaxDom = document.getElementById("add-tax-price");
-    console.log(inputValue);
     addTaxDom.innerHTML = Math.floor(inputValue/10);
     
     const salesProfit = document.getElementById("profit");
-    console.log(inputValue);
     salesProfit.innerHTML = Math.floor(inputValue - (inputValue/10));
   })
 })
