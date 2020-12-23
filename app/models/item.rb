4cb31c62a8a6a41a19741d9a59f93fb26d4de2b0
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-23 14:19:15 +0900

    rubocop使用後

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,17 +10,16 @@ class Item < ApplicationRecord
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
-    validates :quality_id        
-    validates :shipping_cost_id  
-    validates :prefecture_id    
+    validates :quality_id
+    validates :shipping_cost_id
+    validates :prefecture_id
     validates :ship_date_id
-  end      
-  
+  end
+
   with_options presence: true do
     validates :image
     validates :item_name
     validates :explanation
-    validates :price,         numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
-  
 end
