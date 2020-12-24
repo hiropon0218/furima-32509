1437ee00208b7fab30d47c5b021847a26c35f5f5
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-24 12:28:50 +0900

    updateアクションの為のコントローラーとルーティングの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+     if @item.update(item_params)
+      redirect_to item_path
+     else
+      render :edit
+     end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
