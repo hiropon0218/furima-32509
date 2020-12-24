fb523a6654054748cfb63ea7051fe94c0107ea14
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-24 15:17:36 +0900

    重複している記述のメソッド化
    ```
    @item = Item.find(params[:id]
    ```
    をメソッド化しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :params_input, except: [:index, :new, :create]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,12 +19,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -33,8 +33,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
+
 end
 
 private
@@ -42,4 +43,8 @@ private
 def item_params
   params.require(:item).permit(:image, :item_name, :explanation, :category_id, :quality_id, :shipping_cost_id, :prefecture_id,
                                :ship_date_id, :price).merge(user_id: current_user.id)
+end
+
+def params_input
+  @item = Item.find(params[:id])
 end
