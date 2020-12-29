c29e43dfcccd12628af27054f1fae0621515b69f
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2020-12-29 11:38:37 +0900

    カレントユーザーidと商品のidが同一ならばトップページに遷移する機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,6 +3,9 @@ class PurchasesController < ApplicationController
   def index
     @user_shipping = UserShipping.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def create
